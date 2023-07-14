3d8787bcc62fe5ffea884b7832d575d25e3b56b3
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2023-07-14 09:04:24 +0000

    dev: update name of resources in example

--- examples/aws-lambda-fn/main.tf
+++ examples/aws-lambda-fn/main.tf
@@ -1,17 +1,16 @@
-module "packager" {
+module "artifact_packager" {
   source = "../../"
 
   artifact_src_path    = "/tmp/package.zip"
   docker_build_context = "${path.module}/fixures/echo-app"
   docker_build_target  = "package"
-  docker_build_args    = {}
 }
 
 resource "aws_lambda_function" "this" {
   function_name = "tf-example-echo-app"
 
-  filename         = module.packager.artifact_dst_path
-  source_code_hash = filebase64sha256(module.packager.artifact_dst_path)
+  filename         = module.artifact_packager.artifact_dst_path
+  source_code_hash = filebase64sha256(module.artifact_packager.artifact_dst_path)
   handler          = "index.handler"
 
   runtime = "nodejs18.x"
