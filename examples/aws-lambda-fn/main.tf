8b74785d678f33d0cf82da3e53dba860bc361e74
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2023-07-17 17:21:23 +0000

    dev: fix issues with aws-lambda-fn example

--- examples/aws-lambda-fn/main.tf
+++ examples/aws-lambda-fn/main.tf
@@ -1,20 +1,24 @@
 module "artifact_packager" {
   source = "../../"
 
-  artifact_src_path    = "/tmp/package.zip"
-  docker_build_context = "${path.module}/fixures/echo-app"
-  docker_build_target  = "package"
+  artifact_dst_directory = "${path.module}/dist"
+  artifact_src_path      = "/tmp/package.zip"
+  docker_build_context   = "${path.module}/fixtures/echo-app"
+  docker_build_target    = "package"
 }
 
 resource "aws_lambda_function" "this" {
   function_name = "tf-example-echo-app"
 
-  filename         = module.artifact_packager.artifact_dst_path
-  source_code_hash = filebase64sha256(module.artifact_packager.artifact_dst_path)
+  runtime          = "nodejs18.x"
   handler          = "index.handler"
+  filename         = module.artifact_packager.artifact_package_path
+  source_code_hash = filebase64sha256(module.artifact_packager.artifact_package_path)
+  role             = aws_iam_role.this.arn
 
-  runtime = "nodejs18.x"
-  role    = aws_iam_role.this.arn
+  depends_on = [
+    module.artifact_packager,
+  ]
 }
 
 resource "aws_iam_role" "this" {
